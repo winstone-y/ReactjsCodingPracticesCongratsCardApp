d39048ca4768060764d170fae8ef45a4e238cf75
Your Name <you@example.com>
2022-12-05 07:51:57 +0000

    empty preview

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+    <h1 className ="heading" ></h1>
+    <div className="container">
+        <div className="image-container">
+            <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+            />
+        </div>
+        <h1 className="name">Winstone</h1>
+        <p className="college">NxtWave</p>
+        <div className="image-container">
+            <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+            />
+        </div>
+
+    </div>
+  )
+  
+ReactDOM.render(element, document.getElementById('root'))
